7c06a8597626fd43f803ea05b3783a0c7fbdb8ca
Bjarne <bjarne98@gmx.com>
2021-11-17 09:39:22 +0100

    remove Playground from git tracking

--- Playground/b.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import *
-from Playground.DatabaseConnector.base import Base
-
-class B(Base):
-    __tablename__ = "B"
-    id    = Column(Integer, primary_key=True)
-    A_id  = Column(Integer, ForeignKey("AccessLog.id"))
--- Playground/c.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import *
-from Playground.DatabaseConnector.base import Base
-
-
-class C(Base):
-    __tablename__ = "C"
-    id = Column(Integer, primary_key=True)
-    A_id = Column(Integer, ForeignKey("AccessLog.id"))
