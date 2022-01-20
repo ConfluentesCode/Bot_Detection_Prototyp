e74e768cdd93093a561f34c494f941970d848b78
Bjarne <bjarne98@gmx.com>
2022-01-20 17:25:40 +0100

    add request type HEAD

--- Constants/Constants.py
+++ Constants/Constants.py
@@ -3,7 +3,7 @@ DATABASE_PATH = 'sqlite:////Users/bjarneschroeder/PycharmProjects/Bot_Detection_
 
 IP_ADDRESS_REGEX = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
 TIMESTAMP_REGEX = r"\d{2}/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4}"
-HTTP_METHOD_REGEX = r"(POST|DELETE|GET|PUT)"
+HTTP_METHOD_REGEX = r"(POST|DELETE|GET|PUT|HEAD)"
 URL_REGEX = r"(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])"
 RESPONSE_CODE_REGEX = r"303|404|500|403|502|304|200"
 HTTP_VERSION_REGEX = r"HTTP/\d{1}\.\d{1}"
