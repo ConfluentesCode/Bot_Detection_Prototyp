15e1eadd6a6557ef33e176ef3567d645f8609977
Bjarne <bjarne98@gmx.com>
2022-02-03 18:47:23 +0100

    some moving struggle

--- DatabaseConnector/DatabaseSettings.py
+++ DatabaseConnector/DatabaseSettings.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_PATH = 'sqlite:////Users/bjarneschroeder/PycharmProjects/Bot_Detection_Prototyp/DatabaseConnector/Database/BotDetection.db'
+DATABASE_PATH = 'sqlite:////Users/bjarneschroder/PycharmProjects/Bot_Detection_Prototyp/DatabaseConnector/Database/BotDetection.db'
 Base = declarative_base()
 
 engine = create_engine(DATABASE_PATH, echo=False)
