9fe2b08283b13bd5b50ec968ee734e856e9a88ce
Bjarne <bjarne98@gmx.com>
2022-02-10 10:45:22 +0100

    a few explanations

--- DataPreparator/Constants/PathConstants.py
+++ DataPreparator/Constants/PathConstants.py
@@ -1,2 +1,7 @@
+# MUST BE SET!!!
+# set path to your local server access logs source which should be used to train the detection-approach
 ACCESS_LOG_FILE_PATH = '/Users/bjarneschroder/PycharmProjects/Bot_Detection_Prototyp/DataPreparator/InputFiles/logfiles_iran_short.log'
+
+# MUST BE SET!!!
+# set path to your local udger database to use the python udger parser
 UDGER_PATH = '/Users/bjarneschroder/PycharmProjects/Bot_Detection_Prototyp/DatabaseConnector/Database'
