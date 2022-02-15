64b7a5408daafb4ccd8aa2fae6496f3a0f62d0d5
Bjarne <bjarne98@gmx.com>
2022-02-15 09:45:39 +0100

    no-issue

--- DatabaseConnector/Models/DatabaseCreator.py
+++ DatabaseConnector/Models/DatabaseCreator.py
@@ -5,7 +5,7 @@ import Session
 import Request
 
 # Models must be imported here to bind them in .creat_all() method.
-# DatabaseCreator must be in the same folder as Models for unknown reasons
+# SQLAlchemy DatabaseCreator must be in the same folder as Models for unknown reasons
 # TODO Base problem clarification
 
 Base.metadata.create_all(engine, checkfirst=True)
