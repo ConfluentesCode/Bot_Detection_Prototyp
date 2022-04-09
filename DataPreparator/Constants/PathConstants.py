345381efdf719d83d2509c587d633aec0417e899
Bjarne <bjarne98@gmx.com>
2022-04-09 18:50:54 +0200

    remove path constants from project

--- DataPreparator/Constants/PathConstants.py
+++ DataPreparator/Constants/PathConstants.py
@@ -1,7 +1,7 @@
 # MUST BE SET!!!
 # set path to your local server access logs source which should be used to train the detection-approach
-ACCESS_LOG_FILE_PATH = '/Users/bjarneschroder/PycharmProjects/Bot_Detection_Prototyp/DataPreparator/InputFiles/logfiles_iran_short.log'
+ACCESS_LOG_FILE_PATH = ''
 
 # MUST BE SET!!!
 # set path to your local udger database to use the python udger parser
-UDGER_PATH = '/Users/bjarneschroder/PycharmProjects/Bot_Detection_Prototyp/DatabaseConnector/Database'
+UDGER_PATH = ''
